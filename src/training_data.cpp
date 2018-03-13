db7a06a7f8371aca1593719015ee9fe8e8c15300
AshaAgrawal07 <ashaa2@illinois.edu>
2018-03-13 03:08:47 -0500

    working on training portion (calculating probabilities with the laplace factor)

--- src/training_data.cpp
+++ src/training_data.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 #include <iomanip>
 
-void read_input(){
+vector <std::pair<int, Feature_Vector>> read_input() {
     std::string file_data_name;
     std::srting file_answers_name;
     std::istream ins;
@@ -25,24 +25,34 @@ void read_input(){
     training_data_file.open(file_data_name);
     training_answers_file.open(file_answers_name);
     //check that file is valid
-    if(!training_data_file.is_open() || !training_answers_file.is_open() ) {
+    if (!training_data_file.is_open() || !training_answers_file.is_open()) {
         std::cout << "Cannot open file" << std::endl;
         std::exit(1);
     }
 
     //at this point, i know that files will be read
     //now creating a vector of pairs that contains a Feature_Vector and its corresponding classification (int)
-    vector<std::pair<int, Feature_Vector>> images;
+    vector <std::pair<int, Feature_Vector>> images;
     int classification;
     while (training_data_file >> ins && training_answers_file >> classification) {
         Feature_Vector feature;
-        vector<std::pair<int, Feature_Vector>> image;
+        vector <std::pair<int, Feature_Vector>> image;
         image = std::pair<classification, feature.read(ins)>;
         images.push_back(image);
     }
     training_data_file.close();
     std::cout << "Training complete" << std::endl;
+    return images;
+}
+
+void train(vector <std::pair<int, Feature_Vector>> images) {
+    std::vector<std::vector<std::vector>> probabilities;
+
+    //go through images 10 times to find the all of the Frequency_Vectors with the specific classification
+    for (int i = 0; i < 10; i++) {
+        
     }
+}
 
 
 
